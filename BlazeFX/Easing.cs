4a7def24b22e9472ad806fb2bb70b6dc3a247fe4
Matheus Evangelista Dos Santos <matheus.evangelista7@protonmail.com>
2024-10-09 20:34:22 -0300

    feat: add support to all css animations and easing

---
+++ BlazeFX/Easing.cs
@@ -0,0 +1,43 @@
+﻿namespace BlazeFX;
+
+public enum Easing
+{
+	Ease,
+	EaseIn,
+	EaseOut,
+	EaseInOut,
+	Linear,
+	StepStart,
+	StepEnd,
+	EaseInSine,
+	EaseOutSine,
+	EaseInOutSine,
+	EaseInQuad,
+	EaseOutQuad,
+	EaseInOutQuad,
+	EaseInCubic,
+	EaseOutCubic,
+	EaseInOutCubic,
+	EaseInQuart,
+	EaseOutQuart,
+	EaseInOutQuart,
+	EaseInQuint,
+	EaseOutQuint,
+	EaseInOutQuint,
+	EaseInExpo,
+	EaseOutExpo,
+	EaseInOutExpo,
+	EaseInCirc,
+	EaseOutCirc,
+	EaseInOutCirc,
+	EaseInBack,
+	EaseOutBack,
+	EaseInOutBack,
+	EaseInElastic,
+	EaseOutElastic,
+	EaseInOutElastic,
+	EaseInBounce,
+	EaseOutBounce,
+	EaseInOutBounce
+}
+
